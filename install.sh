7d2ee8c5d0941fb2ee1feba3adb161e19c2e1166
MeePwn <maybewaityou@gmail.com>
2023-08-24 13:46:44 +0800

    first commit

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 echo "开始安装"
-mv ./saul* ./saul
-chmod +x ./saul
-mv ./saul* /usr/local/bin/
-echo "安装完成"
+FILE=saul*
+
+if test -e "$FILE"; then
+  echo "====----"
+fi
+
+if [ test -e "$FILE" ]; then
+  mv ./"$FILE" ./saul
+  chmod +x ./saul
+#  mv ./saul /usr/local/bin/
+  mv ./saul ./test/
+  echo "安装完成"
+  exit 0
+fi
+
+echo "可执行文件不存在"
+exit 1
