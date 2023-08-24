58a1497650453df59a10409180cd59a6d36f8e76
MeePwn <maybewaityou@gmail.com>
2023-08-24 12:09:19 +0800

    first commit

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "开始安装"
-echo "===================="
-
 mv ./saul* ./saul
 chmod +x ./saul
-mv ./saul ./test/
-#mv ./saul* /usr/local/bin/
+mv ./saul* /usr/local/bin/
 echo "安装完成"
