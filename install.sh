287107f6bc620b17db6a2e8f017b29da1cf5be93
MeePwn <maybewaityou@gmail.com>
2023-08-24 14:11:36 +0800

    first commit

--- install.sh
+++ install.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-
 echo "开始安装"
-FILE=saul*
+DARWIN_ARM_FILE="saul-darwin-arm64"
+DARWIN_AMD_FILE="saul-darwin-amd64"
+WINDOWS_AMD_FILE="saul-windows-amd64.exe"
+TARGET_FILE="saul"
 
-if test -e "$FILE"; then
-  echo "====----"
+if test -e "./$DARWIN_ARM_FILE"; then
+  echo $DARWIN_ARM_FILE
+  mv $DARWIN_ARM_FILE ./$TARGET_FILE
+elif test -e "./$DARWIN_AMD_FILE"; then
+  echo $DARWIN_AMD_FILE
+  mv $DARWIN_AMD_FILE ./$TARGET_FILE
+elif test -e "./$WINDOWS_AMD_FILE"; then
+  TARGET_FILE="$TARGET_FILE.exe"
+  echo $WINDOWS_AMD_FILE
+  mv $WINDOWS_AMD_FILE ./$TARGET_FILE
+else
+  echo "Error: 可执行文件不存在"
+  exit 1
 fi
 
-if [ test -e "$FILE" ]; then
-  mv ./"$FILE" ./saul
-  chmod +x ./saul
-#  mv ./saul /usr/local/bin/
-  mv ./saul ./test/
-  echo "安装完成"
-  exit 0
-fi
-
-echo "可执行文件不存在"
-exit 1
+chmod +x ./$TARGET_FILE
+mv ./$TARGET_FILE /usr/local/bin/
+echo "安装完成"
+exit 0
